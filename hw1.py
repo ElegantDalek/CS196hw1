a88aa9eebe0d9d284028636322f1bda675a3a357
ElegantDalek <karatedkid@gmail.com>
2018-09-30 20:37:27 -0500

    All working hopefully

--- hw1.py
+++ hw1.py
@@ -8,7 +8,6 @@ import numpy as np
 import math
 
 def histogram_times(filename):
-    import csv
     dataIndex = 10
     with open(filename) as f:
         csv_reader = csv.reader(f)
@@ -41,39 +40,34 @@ def single_type_candy_count(filename):
     numCandies = 0
     for pokemon in pokedex['pokemon']:
         if pokemon['candy'] != "None":
-            numCandies += 1
-        print(pokemon['name'] + ": " + pokemon['candy'])
+            if 'candy_count' in pokemon and len(pokemon['type']) == 1:
+                numCandies += pokemon['candy_count']
     return numCandies
 
 def reflections_and_projections(points):
-    import numpy as np
-    '''
-    TODO: WRITE TEST CASES TO TEST
-    '''
+    
     # Reflects the points over the line y = 1
-    xcoords = points[:,0]
-    xcoords -= 1
-    xcoords *= -1
-    xcoords += 1
-    print(points)
+    ycoords = points[:][1]
+    ycoords -= 1
+    ycoords *= -1
+    ycoords += 1
 
     # Rotates pi / 2 radians
     theta = np.pi / 2 
     matrix_to_multiply = np.array([[np.cos(theta), np.sin(theta) * -1], [np.sin(theta), np.cos(theta)]])
-    points = np.matmul(matrix_to_multiply, points)
-    print(points)
+    rotated_points = points.copy()
+    rotated_points = np.matmul(matrix_to_multiply, points)
 
     # Project on the line y = 3x
     m = 3
-    projection_matrix = np.array([[1, m], [m, m**2]])
     coefficient = 1/(m**2 + 1)
-    print(points)
-    
-    points = np.matmul(projection_matrix, points) * coefficient
-    return points
+    projection_matrix = coefficient * np.array([[1, m], [m, m**2]])
+    projected_points = rotated_points.copy()
+    projected_points = np.matmul(projection_matrix, rotated_points)
+
+    return projected_points
 
 def normalize(image):
-    import numpy as np
     max = np.amax(np.array([np.amax(x) for x in range(len(image))]))
     min = np.amin(np.array([np.amin(x) for x in range(len(image))]))
     coefficient = 255 / (max - min)
@@ -85,13 +79,9 @@ def sigmoid_normalize(image, a):
     return 255*(1+e**((-a)**-1*image-128))**-1
 
 #test_array = np.array([[1, 2], [3, 4]])
-print(histogram_times('airplane_crashes.csv'))
+#print(histogram_times('airplane_crashes.csv'))
 #print(weigh_pokemons('pokedex.json', 10.0))
 print(single_type_candy_count('pokedex.json'))
-#reflections_and_projections(test_array)
 
 
-#b = [x,y for x in range(10) for y in range(10) if y % 2 == 0]
-
-#image_array = np.arange(32 ** 2).reshape(32, 32)
 #print(normalize(image_array))
